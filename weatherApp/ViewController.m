1910f899b4ad4e73146b85ef916035188ea8d01f
bataevvlad <bataevvlad@gmail.com>
2019-05-18 21:33:43 +0300

    no message

--- weatherApp/ViewController.m
+++ weatherApp/ViewController.m
@@ -31,6 +31,11 @@ static const CGFloat   iconHeight = 30;
 @property (strong, nonatomic) UILabel *conditionsLabel;
 @property (strong, nonatomic) UIImageView *iconView;
 
+@property (assign, nonatomic) CGRect temperatureFrame;
+@property (assign, nonatomic) CGRect iconFrame;
+
+
+
 @end
 
 @implementation ViewController
@@ -69,7 +74,11 @@ static const CGFloat   iconHeight = 30;
     self.header.backgroundColor = [UIColor clearColor];
     self.tableView.tableHeaderView = self.header;
     
-    [self hiloMethod];
+    [self hiloOutput];
+    [self temperatureOutput];
+    [self iconOutput];
+    [self conditionsOutput];
+    [self cityLabelOutput];
     
 }
 
@@ -95,18 +104,72 @@ static const CGFloat   iconHeight = 30;
 
 #pragma mark - Frames & Margins
 
-- (void) hiloMethod {
+- (void) hiloOutput {
     
-    CGRect hiloFrame = CGRectMake(inset, self.headerFrame.size.height - (3* hiloHeight), self.headerFrame.size.width + inset, hiloHeight);
+    CGRect hiloFrame = CGRectMake(inset,
+                                  self.headerFrame.size.height - (3* hiloHeight),
+                                  self.headerFrame.size.width + inset,
+                                  hiloHeight);
     
     UILabel *hiloLabel = [[UILabel alloc] initWithFrame:hiloFrame];
     hiloLabel.backgroundColor = [UIColor clearColor];
     hiloLabel.textColor = [UIColor whiteColor];
-    hiloLabel.text = @"0c / 0c";
+    hiloLabel.text = @"0° / 0°";
     hiloLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:28];
     [self.header addSubview:hiloLabel];
 }
 
+- (void) temperatureOutput {
+    
+    self.temperatureFrame = CGRectMake(inset,
+                                         self.headerFrame.size.height - (temperatureHeight + (3* hiloHeight)),
+                                         self.headerFrame.size.width - (2 * inset),
+                                         temperatureHeight);
+    
+    UILabel *temperatureLabel = [[UILabel alloc] initWithFrame:self.temperatureFrame];
+    temperatureLabel.backgroundColor = [UIColor clearColor];
+    temperatureLabel.textColor = [UIColor whiteColor];
+    temperatureLabel.text = @"0°";
+    temperatureLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:120];
+    [self.header addSubview:temperatureLabel];
+}
+
+- (void) iconOutput {
+    
+    self.iconFrame = CGRectMake(inset,
+                                  self.temperatureFrame.origin.y - iconHeight,
+                                  iconHeight,
+                                  iconHeight);
+    
+    UIImageView *iconView = [[UIImageView alloc] initWithFrame:self.iconFrame];
+    iconView.contentMode = UIViewContentModeScaleAspectFit;
+    iconView.backgroundColor = [UIColor clearColor];
+    [self.header addSubview:iconView];
+    //TODO icon;
+}
+
+-(void) conditionsOutput {
+    
+    CGRect conditionsFrame = self.iconFrame;
+    conditionsFrame.size.width = self.view.bounds.size.width - (((2 * inset) + iconHeight) + 10);
+    conditionsFrame.origin.x = self.iconFrame.origin.x + (iconHeight + 10);
+    
+    UILabel *conditionsLabel = [[UILabel alloc] initWithFrame:conditionsFrame];
+    conditionsLabel.backgroundColor = [UIColor clearColor];
+    conditionsLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:18];
+    conditionsLabel.textColor = [UIColor whiteColor];
+    [self.header addSubview:conditionsLabel];
+}
+
+- (void) cityLabelOutput {
+    UILabel *cityLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, self.view.bounds.size.width, 30)];
+    cityLabel.backgroundColor = [UIColor clearColor];
+    cityLabel.textColor = [UIColor whiteColor];
+    cityLabel.text = @"One moment...";
+    cityLabel.font = [UIFont fontWithName:@"HelveticaNeue" size:25];
+    cityLabel.textAlignment = NSTextAlignmentCenter;
+    [self.header addSubview:cityLabel];
+}
 
 #pragma mark - UITableViewDataSource
 
